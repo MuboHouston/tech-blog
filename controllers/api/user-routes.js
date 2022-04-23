9c656d7adb910cdff2f9e82b886b5770c44264a4
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-04-23 11:08:32 -0500

    create connection to server and user post route

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// POST /api/users
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+        
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
