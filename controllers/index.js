e6fd351817a427e2cb4d9a1585d1836574f44963
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-04-24 02:37:26 -0500

    add single post page

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,8 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes')
 // const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 // router.use('/dashboard', dashboardRoutes);
 
 //if a client makes an request to an endpoint that does not exist, they will receive a 404 error.
